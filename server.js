3441e1d1a303aa4674c7c3769a37bcfb6fd4e4f2
Rich Rodman <rrodman@rrodman-ltmfqcl.internal.salesforce.com>
2026-08-05 10:55:33 -0400

    Initial commit: Heinz Verified loyalty microsite

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs   = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const MIME = {
+  '.html': 'text/html',
+  '.css':  'text/css',
+  '.js':   'text/javascript',
+  '.png':  'image/png',
+  '.jpg':  'image/jpeg',
+  '.svg':  'image/svg+xml',
+  '.ico':  'image/x-icon',
+};
+
+http.createServer((req, res) => {
+  let urlPath = req.url === '/' ? '/index.html' : req.url;
+  const filePath = path.join(__dirname, urlPath);
+  const ext = path.extname(filePath);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': MIME[ext] || 'text/plain' });
+    res.end(data);
+  });
+}).listen(PORT, () => console.log(`Serving on port ${PORT}`));
